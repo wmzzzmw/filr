f600d329fbd0b1428bfbb6664b1aea1d6fce2067
wmzzzmw <93494481+wmzzzmw@users.noreply.github.com>
2021-11-01 01:00:12 +0300

    Add files via upload

---
+++ ustfin.online/form/js/lead.js
@@ -0,0 +1,227 @@
+var list_of_countries = {AU:'Австралия', AT:'Австрия', AZ:'Азербайджан', AX:'Аландские острова', AL:'Албания', DZ:'Алжир', VI:'Виргинские Острова (США)', AS:'Американское Самоа', AI:'Ангилья', AO:'Ангола', AD:'Андорра', AQ:'Антарктика', AG:'Антигуа и Барбуда', AR:'Аргентина', AM:'Армения', AW:'Аруба', AF:'Афганистан', BS:'Багамские Острова', BD:'Бангладеш', BB:'Барбадос', BH:'Бахрейн', BZ:'Белиз', BY:'Белоруссия', BE:'Бельгия', BJ:'Бенин', BM:'Бермуды', BG:'Болгария', BO:'Боливия', BQ:'Бонэйр, Синт-Эстатиус и Саба', BA:'Босния и Герцеговина', BW:'Ботсвана', BR:'Бразилия', IO:'Британская территория в Индийском океане', VG:'Виргинские Острова (Великобритания)', BN:'Бруней', BF:'Буркина-Фасо', BI:'Бурунди', BT:'Бутан', VU:'Вануату', VA:'Ватикан', GB:'Великобритания', HU:'Венгрия', VE:'Венесуэла', UM:'Внешние малые острова США', TL:'Восточный Тимор', VN:'Вьетнам', GA:'Габон', HT:'Гаити', GY:'Гайана', GM:'Гамбия', GH:'Гана', GP:'Гваделупа', GT:'Гватемала', GF:'Гвиана', GN:'Гвинея', GW:'Гвинея-Бисау', DE:'Германия', GG:'Гернси', GI:'Гибралтар', HN:'Гондурас', HK:'Гонконг', GD:'Гренада', GL:'Гренландия', GR:'Греция', GE:'Грузия', GU:'Гуам', DK:'Дания', JE:'Джерси', DJ:'Джибути', DM:'Доминика', DO:'Доминиканская Республика', CD:'ДР Конго', EU:'Европейский союз', EG:'Египет', ZM:'Замбия', EH:'САДР', ZW:'Зимбабве', IL:'Израиль', IN:'Индия', ID:'Индонезия', JO:'Иордания', IQ:'Ирак', IR:'Иран', IE:'Ирландия', IS:'Исландия', ES:'Испания', IT:'Италия', YE:'Йемен', CV:'Кабо-Верде', KZ:'Казахстан', KY:'Острова Кайман', KH:'Камбоджа', CM:'Камерун', CA:'Канада', QA:'Катар', KE:'Кения', CY:'Кипр', KG:'Киргизия', KI:'Кирибати', TW:'Китайская Республика', KP:'КНДР(Корейская Народно-Демократическая Республика)', CN:'Китай(Китайская Народная Республика)', CC:'Кокосовые острова', CO:'Колумбия', KM:'Коморы', CR:'Коста-Рика', CI:'Кот-д’Ивуар', CU:'Куба', KW:'Кувейт', CW:'Кюрасао', LA:'Лаос', LV:'Латвия', LS:'Лесото', LR:'Либерия', LB:'Ливан', LY:'Ливия', LT:'Литва', LI:'Лихтенштейн', LU:'Люксембург', MU:'Маврикий', MR:'Мавритания', MG:'Мадагаскар', YT:'Майотта', MO:'Макао', MK:'Северная Македония', MW:'Малави', MY:'Малайзия', ML:'Мали', MV:'Мальдивы', MT:'Мальта', MA:'Марокко', MQ:'Мартиника', MH:'Маршалловы Острова', MX:'Мексика', FM:'Микронезия', MZ:'Мозамбик', MD:'Молдавия', MC:'Монако', MN:'Монголия', MS:'Монтсеррат', MM:'Мьянма', NA:'Намибия', NR:'Науру', NP:'Непал', NE:'Нигер', NG:'Нигерия', NL:'Нидерланды', NI:'Никарагуа', NU:'Ниуэ', NZ:'Новая Зеландия', NC:'Новая Каледония', NO:'Норвегия', AE:'ОАЭ', OM:'Оман', BV:'Остров Буве', IM:'Остров Мэн', CK:'Острова Кука', NF:'Остров Норфолк', CX:'Остров Рождества', PN:'Острова Питкэрн', SH:'Острова Святой Елены, Вознесения и Тристан-да-Кунья', PK:'Пакистан', PW:'Палау', PS:'Государство Палестина', PA:'Панама', PG:'Папуа — Новая Гвинея', PY:'Парагвай', PE:'Перу', PL:'Польша', PT:'Португалия', PR:'Пуэрто-Рико', CG:'Республика Конго', KR:'Республика Корея', RE:'Реюньон', RU:'Россия', RW:'Руанда', RO:'Румыния', SV:'Сальвадор', WS:'Самоа', SM:'Сан-Марино', ST:'Сан-Томе и Принсипи', SA:'Саудовская Аравия', SZ:'Эсватини', MP:'Северные Марианские Острова', SC:'Сейшельские Острова', BL:'Сен-Бартелеми (Карибы)', MF:'Сен-Мартен', PM:'Сен-Пьер и Микелон', SN:'Сенегал', VC:'Сент-Винсент и Гренадины', KN:'Сент-Китс и Невис', LC:'Сент-Люсия', RS:'Сербия', SG:'Сингапур', SX:'Синт-Мартен', SY:'Сирия', SK:'Словакия', SI:'Словения', SB:'Соломоновы Острова', SO:'Сомали', SD:'Судан', SR:'Суринам', US:'США', SL:'Сьерра-Леоне', TJ:'Таджикистан', TH:'Таиланд', TZ:'Танзания', TC:'Теркс и Кайкос', TG:'Того', TK:'Токелау', TO:'Тонга', TT:'Тринидад и Тобаго', TV:'Тувалу', TN:'Тунис', TM:'Туркмения', TR:'Турция', UG:'Уганда', UZ:'Узбекистан', UA:'Украина', WF:'Уоллис и Футуна', UY:'Уругвай', FO:'Фарерские острова', FJ:'Фиджи', PH:'Филиппины', FI:'Финляндия', FK:'Фолклендские острова', FR:'Франция', PF:'Французская Полинезия', TF:'Французские Южные и Антарктические территории', HM:'Херд и Макдональд', HR:'Хорватия', CF:'ЦАР', TD:'Чад', ME:'Черногория', CZ:'Чехия', CL:'Чили', CH:'Швейцария', SE:'Швеция', SJ:'Шпицберген и Ян-Майен', LK:'Шри-Ланка', EC:'Эквадор', GQ:'Экваториальная Гвинея', ER:'Эритрея', EE:'Эстония', ET:'Эфиопия', ZA:'ЮАР', GS:'Южная Георгия и Южные Сандвичевы Острова', SS:'Южный Судан', JM:'Ямайка', JP:'Япония'};
+
+(function($) {
+	'use strict';
+
+	function setCookie(key, value) {
+		var expires = new Date();
+		expires.setTime(expires.getTime() + 1 * 3600 * 1000);
+		document.cookie = key + '=' + value + ';path=/' + ';expires=' + expires.toUTCString();
+	}
+    function getCookie(key) {
+        var keyValue = document.cookie.match('(^|;) ?' + key + '=([^;]*)(;|$)');
+        return keyValue ? keyValue[2] : null;
+    }
+
+	var queryString = window.location.search,
+		utm_source = '';
+	if ( queryString ) {
+		var urlParams = new URLSearchParams(queryString),
+		utm_source = urlParams.get('utm_source');
+
+		if ( utm_source )
+			setCookie('utm_source', utm_source);
+		else
+			utm_source = '';
+	}
+	else {
+		if ( getCookie('utm_source') )
+			utm_source = getCookie('utm_source');
+	}
+
+	if ( !getCookie('usr_country') ) {
+		$.get('https://ipinfo.io?token=3704e1799ba275', function(response) {
+			if ( response.country && response.city ) {
+				setCookie('usr_country', response.country);
+				window.usr_country = response.country;
+			}
+		}, 'jsonp');
+	}
+	else
+		window.usr_country = getCookie('usr_country');
+
+	window.onload = function() {
+
+
+		var telInput = $('input[name="phone"]'),
+			onlycountries = ['ru', 'kz'];
+
+		if ( $.fn.intlTelInput ) {
+			if ( window.usr_country && window.usr_country != 'RU' && window.usr_country != 'KZ' ) {
+				onlycountries = ['al', 'ad', 'at', 'by', 'be', 'ba', 'bg', 'hr', 'cz', 'dk', 'ee', 'fo', 'fi', 'fr', 'de', 'gi', 'gr', 'va', 'hu', 'is', 'ie', 'it', 'lv', 'li', 'lt', 'lu', 'mk', 'mt', 'md', 'mc', 'me', 'nl', 'no', 'pl', 'pt', 'ro', 'sm', 'rs', 'sk', 'si', 'es', 'se', 'ch', 'gb'];
+			}
+
+			telInput.intlTelInput({
+				separateDialCode: true,
+				initialCountry: 'auto',
+				geoIpLookup: function(success, failure) {
+					if ( window.usr_country && onlycountries.includes(window.usr_country.toLowerCase()) )
+						success(window.usr_country);
+				},
+				onlyCountries: onlycountries,
+				utilsScript: '/js/utils.js',
+			});
+
+			telInput.on( 'change keyup blur', function() {
+				if ( $(this).intlTelInput('isValidNumber') ) {
+					$(this).removeClass('err');
+					$(this).parents('form').find('[type="submit"]').removeAttr('disabled');
+				}
+				else {
+					$(this).addClass('err');
+					$(this).parents('form').find('[type="submit"]').attr('disabled', 'disabled');
+				}
+			});
+
+			telInput.on( 'keypress', function() {
+				if ( $(this).val().replace(/\s/g,'').replace(/-/g, '').length + 1 > $(this).attr('placeholder').replace(/\s/g,'').replace(/-/g, '').length )
+					$(this).val($(this).val().slice(0, -1));
+			});
+
+			telInput.on('countrychange', function() {
+				$(this).val('');
+			});
+		}
+
+	};
+
+	$('.form-simple form').submit(function(e){
+
+
+
+		e.preventDefault();
+		var $form = $(this),
+			country = list_of_countries[$form.find('input[name="phone"]').intlTelInput('getSelectedCountryData').iso2.toUpperCase()];
+		var $formCountry = $(this),
+			countryCode = $formCountry.find('input[name="phone"]').intlTelInput('getSelectedCountryData').iso2.toUpperCase();
+
+		$form.find('button[type="submit"]').addClass('loading');
+
+
+
+		$.ajax({
+        			url: window.location.origin + '/lead.php',
+        			type: 'POST',
+        			data: {lead_data: { name: $form.find('input[name="name"]').val(),
+        					email: $form.find('input[name="email"]').val(),
+        					phone: $form.find('input[name="phone"]').intlTelInput('getNumber'),
+        					ad_campaign: utm_source ? utm_source + ',' + country : country,
+        					campaign: 'Юст',
+        					utm: utm_source,
+        					country: country,
+        					countryCode: countryCode,
+        			}},
+        			success: function(data){
+        				$.ajax({
+        					url: 'https://leadstoragecrm.com/api',
+        					type: 'POST',
+        					data: {json: JSON.stringify({
+        									token: '4uLMHvm9s3R5zj9BwXcXq2kPWmyIFJ7A',
+        									full_name: $form.find('input[name="first_name"]').val(),
+        									email: $form.find('input[name="email"]').val(),
+        									phone: $form.find('input[name="phone"]').intlTelInput('getNumber'),
+        									ad_campaign: utm_source,
+        									campaign: 'Горячий',
+        									source: 'Горячий',
+
+        								})
+        							},
+        					crossDomain: true,
+
+
+        					success: function(data){
+
+
+        					window.location.replace('/thank.html');
+        					},
+        				});
+        			},
+        		});
+
+
+        		window.location.replace('/thank.html');
+
+
+
+
+	});
+
+	// QUIZ
+	$('.plquiz-sidebar').click(function() {
+		$('#form-plquiz').find('.active').removeClass('active').end().find('.step-1').addClass('active');
+	});
+
+	$('#form-plquiz .btn').click(function(e) {
+		var step = $(this).parents('.steps');
+
+		if ( step.hasClass('last') || step.find('input[type="text"]').val() == '' ) return;
+
+		else if ( step.hasClass('step-3') ) {
+			step.addClass('loading');
+			setTimeout(function(){
+				step.removeClass('loading active').next().hide().fadeIn().addClass('active');
+			}, 1500);
+		}
+		else
+			step.removeClass('active').next().hide().fadeIn().addClass('active');
+	});
+
+	$('#form-plquiz form').submit(function(e){
+		e.preventDefault();
+		var $form = $(this),
+			country = list_of_countries[$form.find('input[name="phone"]').intlTelInput('getSelectedCountryData').iso2.toUpperCase()];
+		var $formCountry = $(this),
+			countryCode = $formCountry.find('input[name="phone"]').intlTelInput('getSelectedCountryData').iso2.toUpperCase();
+
+		$form.find('input[type="submit"]').addClass('loading');
+
+		$.ajax({
+			url: window.location.origin + '/lead.php',
+			type: 'POST',
+			data: {lead_data: { name: $form.find('input[name="your_name"]').val(),
+					email: $form.find('input[name="email"]').val(),
+					phone: $form.find('input[name="phone"]').intlTelInput('getNumber'),
+					ad_campaign: utm_source ? utm_source + ',' + country  : country,
+					campaign: 'Горячий',
+					utm: utm_source,
+					country: country,
+					countryCode: countryCode,
+			}},
+			success: function(data){
+				$.ajax({
+					url: 'https://leadstoragecrm.com/api',
+					type: 'POST',
+					data: {json: JSON.stringify({
+								token: '4uLMHvm9s3R5zj9BwXcXq2kPWmyIFJ7A',
+								full_name: $form.find('input[name="your_name"]').val(),
+								email: $form.find('input[name="email"]').val(),
+								phone: $form.find('input[name="phone"]').intlTelInput('getNumber'),
+								ad_campaign: utm_source,
+								campaign: 'Горячий',
+								source: 'Горячий',
+								})
+							},
+					crossDomain: true,
+					success: function(data){
+						$form.find('input[type="submit"]').removeClass('loading');
+						$('#form-plquiz .last').removeClass('active').next().hide().fadeIn().addClass('active');
+					},
+				});
+			},
+		});
+
+
+		window.location.replace('/thank.html');
+
+	});
+
+	$(document).mouseleave(function(){
+		if ( ! getCookie('exit_popup') ) {
+			setCookie('exit_popup', '1');
+			$('.plquiz-sidebar').click();
+		}
+	});
+
+
+
+})(jQuery);
